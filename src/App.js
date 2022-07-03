a1de450d3a997ccb524976ba9124536c4808c65d
nikolai-churakov <cni.zip@gmail.com>
2022-07-03 14:16:02 +0300

    remove test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
   return (
     <div className="App">
         <Layout>
-            <div style={{width: 400, border: '1px solid black'}}>Hi react</div>
+            <div style={{width: 400, border: '1px solid black'}}>
+                <h1>Hi react</h1>
+
+            </div>
         </Layout>
 
     </div>
