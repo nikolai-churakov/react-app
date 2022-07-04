05ea6ab6b05318c5f498c2920b3b6690c425a73b
nikolai-churakov <cni.zip@gmail.com>
2022-07-04 20:10:56 +0300

    remove test

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="App">
         <Layout>
-            <Quiz/>
             {/*<div style={{width: 400, border: '1px solid black'}}>*/}
             {/*    <h1>Hi react</h1>*/}
             {/*</div>*/}
+            <Quiz/>
+
         </Layout>
 
     </div>
