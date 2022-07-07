630afe814b3c5236aed0110be5dc64e7874966ec
nikolai-churakov <cni.zip@gmail.com>
2022-07-07 19:58:02 +0300

    Add ActiveQuiz

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,14 +4,18 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component {
     state = {
-        quiz: []
+        quiz: [
+            {text: 'Вопрос 1'},
+            {text: 'Вопрос 2'},
+            {text: 'Вопрос 3'},
+            {text: 'Вопрос 4'}
+        ]
     }
     render() {
         return (
             <div className={'Quiz'}>
+                <h1>Quiz</h1>
                 <div className={'QuizWrapper'}>
-                    <h1>Quiz</h1>
-
                     <ActiveQuiz/>
                 </div>
             </div>
