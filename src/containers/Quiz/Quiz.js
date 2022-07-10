458ac0af3c451a273144338f94f4676a834648c2
nikolai-churakov <cni.zip@gmail.com>
2022-07-10 14:54:21 +0300

    add in state question 2

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,15 +4,28 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component {
     state = {
+        activeQuestion: 0,
         quiz: [
             {
                 question: 'Какого цвета небо?',
                 rightAnswerId: 2,
+                id: 1,
                 answers: [
                     {text: 'Черного', id: 1},
                     {text: 'Синего', id: 2},
                     {text: 'Красного', id: 3},
                     {text: 'Зеленого', id: 4}
+                ]
+            },
+            {
+                question: 'В каком году основали СПБ?',
+                rightAnswerId: 3,
+                id: 2,
+                answers: [
+                    {text: '1700', id: 1},
+                    {text: '1702', id: 2},
+                    {text: '1703', id: 3},
+                    {text: '1803', id: 4}
                 ]
             }
         ]
@@ -30,6 +43,8 @@ class Quiz extends Component {
                     answers={this.state.quiz[0].answers}
                     question={this.state.quiz[0].question}
                     onAnswerClick={this.onAnswerClickHandler}
+                    quizLength={this.state.quiz.length}
+                    answerNumber={this.state.activeQuestion + 1}
                     />
                 </div>
             </div>
