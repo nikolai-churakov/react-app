d67e9cddd7c53002f6a03a735061887913551384
nikolai-churakov <cni.zip@gmail.com>
2022-07-14 21:32:53 +0300

    finish 10parts

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -34,7 +34,7 @@ class Quiz extends Component {
 
     onAnswerClickHandler = answerId => {
         if (this.state.answerState) {
-            const key = Object.key(this.state.answerState) [0]
+            const key = Object.keys(this.state.answerState) [0]
             if (this.state.answerState[key] === 'success') {
                 return
             }
