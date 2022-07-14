ffda2ce32f2f6b726303859ac46777d0298f5072
nikolai-churakov <cni.zip@gmail.com>
2022-07-14 21:07:29 +0300

    add check

--- src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -2,12 +2,19 @@ import React from "react"
 import "./AnswerItem.css"
 
 const AnswerItem = props => {
+    const cls = ['AnswerItem']
+    console.log(cls, 'массив до пуша')
+    console.log(props.state, 'проп стейт')
 
-
+    if (props.state) {
+        cls.push(props.state)
+        console.log(cls, 'масив после пуша')
+    }
 
     return (
         <li
-            className={'AnswerItem'}
+            // className={'AnswerItem'}
+            className={cls.join(' ')}
             onClick={() => props.onAnswerClick(props.answer.id)}
             >
             {props.answer.text}
