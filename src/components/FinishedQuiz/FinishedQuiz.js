0169b089541d66906832fc6ebfa0637e9a15159a
nikolai-churakov <cni.zip@gmail.com>
2022-07-14 22:10:42 +0300

    finish 10parts

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./FinishedQuiz.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
 const FinishedQuiz = props => {
     return (
         <div className={'FinishedQuiz'}>
@@ -7,9 +10,14 @@ const FinishedQuiz = props => {
                 <li>
                     <strong>1. </strong>
                     How a you?
-                    <i className={''} />
+                    <i className={'fa fa-times'} />
+
                 </li>
             </ul>
+            <FontAwesomeIcon icon={solid('user-secret')} />
+            <FontAwesomeIcon icon={regular('coffee')} />
+            {/*<FontAwesomeIcon icon={brands('twitter')} />*/}
+            {/*<FontAwesomeIcon icon={faSquare} />*/}
             <p> Правильно 4 из 10</p>
             <div>
                 <button>Repeat</button>
