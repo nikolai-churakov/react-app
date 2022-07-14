482186674c5750c0dbfb23a312ffc0a1e3f3f4e5
nikolai-churakov <cni.zip@gmail.com>
2022-07-14 21:07:53 +0300

    add check

--- src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -3,17 +3,13 @@ import "./AnswerItem.css"
 
 const AnswerItem = props => {
     const cls = ['AnswerItem']
-    console.log(cls, 'массив до пуша')
-    console.log(props.state, 'проп стейт')
 
     if (props.state) {
         cls.push(props.state)
-        console.log(cls, 'масив после пуша')
     }
 
     return (
         <li
-            // className={'AnswerItem'}
             className={cls.join(' ')}
             onClick={() => props.onAnswerClick(props.answer.id)}
             >
