8dd60f586be7d5a849b86193cab93e37da7b474b
nikolai-churakov <cni.zip@gmail.com>
2022-07-18 22:57:36 +0300

    finish 11

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./FinishedQuiz.css";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { solid, regular} from '@fortawesome/fontawesome-svg-core/import.macro'
 
 
 const FinishedQuiz = props => {
@@ -9,7 +11,7 @@ const FinishedQuiz = props => {
                <li>
                    <strong>1.</strong>
                    How are you
-                   <i className={''}></i>
+                   {/*<FontAwesomeIcon icon={solid('user-secret')} />*/}
                </li>
            </ul>
 
