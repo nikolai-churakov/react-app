744b0e9311af34ec32b0e3c7c0308fe836052c37
nikolai-churakov <cni.zip@gmail.com>
2022-07-19 21:45:31 +0300

    fix bug for answer error

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -8,37 +8,41 @@ const FinishedQuiz = props => {
     return (
         <div className={'FinishedQuiz'}>
            <ul>
-               {/*{props.quiz.map((quizItem, index) => {*/}
 
-               {/*    const  cls = [*/}
-               {/*        props.results[quizItem.id] === 'error' ? '\u0058' : '\u2713',*/}
-               {/*        props.results[quizItem.id]*/}
-               {/*    ]*/}
+               {props.quiz.map((quizItem, index) => {
 
-               {/*    return (*/}
-               {/*        <li*/}
-               {/*            key={index}*/}
-               {/*        >*/}
-               {/*         <strong>{index + 1}</strong>.&nbsp;*/}
-               {/*            { quizItem }*/}
-               {/*            <span>{cls.join(' ')}</span>*/}
+                   const  cls = [
+                       props.results[quizItem.id] === 'error' ? '\u0058' : '\u2713',
+                       props.results[quizItem.id]
+                   ]
+                   console.log(props.results[quizItem.id])
 
-               {/*        </li>*/}
-               {/*    )*/}
+                   return (
+                       <li
+                           key={index}
+                       >
+                        <strong>{index + 1}</strong>.&nbsp;
+                           <p>112</p>
+                           {/*{quizItem}*/}
+                           <span>{cls.join(' ')}</span>
 
-               {/*})}*/}
+                       </li>
+                   )
 
+               })}
 
-               <li>
-                   <strong>1.</strong>
-                   How are you <span className={'error1'}>{'\u0058'}</span>
-                   {/*<FontAwesomeIcon icon={solid('user-secret')} />*/}
-               </li>
-               <li>
-                   <strong>2.</strong>
-                   How are you <span className={'success1'}>{'\u2713'}</span>
-                   {/*<FontAwesomeIcon icon={solid('user-secret')} />*/}
-               </li>
+               {/*Old Variant finished*/}
+               {/*<li>*/}
+               {/*    <strong>1.</strong>*/}
+               {/*    How are you <span className={'error1'}>{'\u0058'}</span>*/}
+               {/*    /!*<FontAwesomeIcon icon={solid('user-secret')} />*!/*/}
+               {/*</li>*/}
+               {/*<li>*/}
+               {/*    <strong>2.</strong>*/}
+               {/*    How are you <span className={'success1'}>{'\u2713'}</span>*/}
+               {/*    /!*<FontAwesomeIcon icon={solid('user-secret')} />*!/*/}
+               {/*</li>*/}
+
            </ul>
 
            <p> Правильно 4 из 10</p>
