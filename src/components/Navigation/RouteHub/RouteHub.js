0fd3ba28df86776b2dab06d71d682e9128dc3650
nikolai-churakov <cni.zip@gmail.com>
2022-07-25 21:14:27 +0300

    add BrowserRouter

--- src/components/Navigation/RouteHub/RouteHub.js
+++ src/components/Navigation/RouteHub/RouteHub.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
 import "./RouteHub.css"
+import {Route, Router} from "react-router-dom";
 
-const RouteHub = props => {
+class RouteHub extends Component {
+    render() {
 
     return (
-        <nav className={'RouteHub'}>
-            <ul>
-                <li>
-                    <a href="/">Easy</a>
-                </li>
-                <li>
-                    <a href="/">Medium</a></li>
-                <li>
-                    <a href="/">Hard</a>
-                </li>
-            </ul>
-        </nav>
-
-    )
+        <div>
+            <nav className={'RouteHub'}>
+                <ul>
+                    <li>
+                        <a href="/easy">Easy</a>
+                    </li>
+                    <li>
+                        <a href="/medium">Medium</a></li>
+                    <li>
+                        <a href="/hard">Hard</a>
+                    </li>
+                </ul>
+            </nav>
+            <Router path="/" render={() => <h1>Home</h1>} />
+        </div>
+        );
+    }
 }
 
 export default RouteHub
