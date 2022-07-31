98e5fde3b74f63882e0d31585e110bf45fe48a4d
nikolai-churakov <cni.zip@gmail.com>
2022-07-31 18:46:22 +0300

    add auth form style

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,14 +1,41 @@
 import React, {Component} from "react";
 import "./Auth.css"
+import Button from "../../components/UI/Button/Button";
 
 export default class Auth extends Component {
+
+    loginHandler = () => {
+
+}
+    registerHandler = () => {
+
+}
+
+    submitHandler = (event) => {
+event.preventDefault()
+    }
+
     render() {
         return (
             <div className={'Auth'}>
                 <div>
                     <h1>Авторизация</h1>
 
-                    {/*<Form></Form>*/}
+                    <form onSubmit={this.submitHandler}>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <button
+                            type="success"
+                            onClick={this.loginHandler}
+                        >
+                            Войти</button>
+
+                        <button
+                            type="primary"
+                            onClick={this.registerHandler}
+                        >
+                            Войти</button>
+                    </form>
                 </div>
 
             </div>
