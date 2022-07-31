c84d72e15b9b42d9983e76cc4762bb487f018478
nikolai-churakov <cni.zip@gmail.com>
2022-07-31 18:36:23 +0300

    add button link to test list

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,16 @@
 import React, {Component} from "react";
+import "./Auth.css"
 
 export default class Auth extends Component {
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={'Auth'}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    {/*<Form></Form>*/}
+                </div>
+
             </div>
         )
     }
