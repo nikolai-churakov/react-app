adf889da793c274f407fa175d2fed5a7f13d39d8
nikolai-churakov <cni.zip@gmail.com>
2022-07-31 22:36:56 +0300

    add formControl12

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -46,8 +46,41 @@ export default class Auth extends Component {
 event.preventDefault()
     }
 
+    validateControl(value, validation) {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (validation.email) {
+
+        }
+
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+
+        return isValid
+    }
     onChangeHandler = (event, controlName) => {
         console.log(`${controlName}: `, event.target.value)
+
+        const formControls = {...this.state.formControls}
+        const control = {...formControls[controlName]}
+
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] =control
+        this.setState({
+            formControls
+        })
 
     }
 
