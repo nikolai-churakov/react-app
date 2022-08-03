2d6b74945b104e4d1e2968034ae971752a461989
nikolai-churakov <cni.zip@gmail.com>
2022-08-03 06:49:45 +0300

    add roodReducer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {createStore} from "redux";
 
+
+const store = createStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
