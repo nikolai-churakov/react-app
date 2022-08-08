09c0c1c68791aae1ba14893fad674f1c1451eb68
nikolai-churakov <cni.zip@gmail.com>
2022-08-08 22:02:07 +0300

    add sing in add reg firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -38,12 +38,33 @@ export default class Auth extends Component {
         }
     }
 
-    loginHandler = () => {
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true,
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[AIzaSyByFMWI3BS4B1Df29-KUApdSAfIznlSMIU]', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
+
 
 }
     registerHandler = async () => {
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[AIzaSyByFMWI3BS4B1Df29-KUApdSAfIznlSMIU]')
-
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true,
+        }
+        try {
+        const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[AIzaSyByFMWI3BS4B1Df29-KUApdSAfIznlSMIU]', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
 }
 
     submitHandler = (event) => {
